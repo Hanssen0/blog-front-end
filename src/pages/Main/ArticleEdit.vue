5a283c94bd298f39b361f8166d0ed64e4e3dede4
handsome0hell <handsome0hell@gmail.com>
2020-07-20 23:03:15 +0800

    Finish article editing

--- src/pages/Main/ArticleEdit.vue
+++ src/pages/Main/ArticleEdit.vue
@@ -4,28 +4,50 @@
       <header class="col-12">
         <label class="w-100">
           <p class="ml-2 mb-1 text-secondary">标题</p>
-          <input class="form-control" v-model="header" />
+          <input class="form-control" v-model="title" />
         </label>
       </header>
       <section class="col-12 mt-2">
         <label class="w-100">
           <p class="ml-2 mb-1 text-secondary">正文</p>
-          <textarea class="form-control" v-model="article" rows="16" />
+          <textarea class="form-control" v-model="content" rows="16" />
         </label>
       </section>
     </article>
     <div class="row mt-2">
-      <button class="col-auto mx-auto btn btn-primary">提交</button>
+      <button class="col-auto mx-auto btn btn-primary" @click="Submit">
+        提交
+      </button>
     </div>
   </div>
 </template>
 <script>
+import {GetArticle, EditArticle} from "@/Network.js";
 export default {
   data: function() {
     return {
-      header: "",
-      article: ""
+      title: "",
+      content: "",
     };
+  }, mounted: function() {
+    GetArticle(
+      article => {
+        this.title = article.title;
+        this.content = article.content;
+      }, {id: 0}
+    );
+  }, methods: {
+    Submit: function() {
+      EditArticle(() => {},
+        {
+          id: 0,
+          article: {
+            title: this.title,
+            content: this.content,
+          }
+        }
+      );
+    }
   }
 };
 </script>
