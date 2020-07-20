2b11f0e91c5b5d76cfefaf1c2ab9a857dd49c017
handsome0hell <handsome0hell@gmail.com>
2020-07-20 22:42:45 +0800

    Tweak layout of article edit page

--- src/pages/Main/ArticleEdit.vue
+++ src/pages/Main/ArticleEdit.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="container shadow">
-    <article class="row">
-      <div class="col-12">
-        <textarea class="form-control" v-model="article" rows="16" />
-      </div>
-      <button class="col-auto mx-auto mt-2 btn btn-primary">
-        提交
-      </button>
+    <article class="row mt-4">
+      <header class="col-12">
+        <label class="w-100">
+          <p class="ml-2 mb-1 text-secondary">标题</p>
+          <input class="form-control" v-model="header" />
+        </label>
+      </header>
+      <section class="col-12 mt-2">
+        <label class="w-100">
+          <p class="ml-2 mb-1 text-secondary">正文</p>
+          <textarea class="form-control" v-model="article" rows="16" />
+        </label>
+      </section>
     </article>
+    <div class="row mt-2">
+      <button class="col-auto mx-auto btn btn-primary">提交</button>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data: function() {
     return {
+      header: "",
       article: ""
     };
   }
