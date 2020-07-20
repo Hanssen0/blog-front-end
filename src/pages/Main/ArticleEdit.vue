13b519e6c6cc5a9e5b1515ae801fe89e18ab0161
handsome0hell <handsome0hell@gmail.com>
2020-07-20 22:27:55 +0800

    Tweak article edit page layout

--- src/pages/Main/ArticleEdit.vue
+++ src/pages/Main/ArticleEdit.vue
@@ -1,21 +1,20 @@
 <template>
-    <div class="container shadow">
-      <div class="container">
-        <br>
-        <span>在这里编辑文章：</span>
-        <p style="white-space: pre-line;">{{ article }}</p>
-        <textarea class="container shadow" v-model="article"
-                  placeholder="Your article here..."></textarea>
-        <button class="btn btn-primary" type="submit"
-                name="" value="">提交</button>
+  <div class="container shadow">
+    <article class="row">
+      <div class="col-12">
+        <textarea class="form-control" v-model="article" rows="16" />
       </div>
-    </div>
+      <button class="col-auto mx-auto mt-2 btn btn-primary">
+        提交
+      </button>
+    </article>
+  </div>
 </template>
 <script>
 export default {
   data: function() {
     return {
-      article : ""
+      article: ""
     };
   }
 };
